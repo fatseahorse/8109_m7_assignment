1297ba9090c6538a224b2f960de13ef9c85adefa
Fat Seahorse <28702633+fatseahorse@users.noreply.github.com>
2026-07-07 08:27:48 +0000

    add create

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const ejs = require('ejs'); // ejs is a template library
+
+// ejs is a template library
+// it allows us to store html in a file and then send it as back as response
+const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
-require('dotenv').config(); // read in our .env file
-
 const app = express();
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true}));
-app.set('layout', 'layouts/base');
 
-const {createPool} = require('mysql2/promise');
+// read in our .env file
+require("dotenv").config();
+const { createPool } = require('mysql2/promise');
 
+app.use(expressLayouts)
+
+// tell Express that we are using ejs
+app.set("view engine", "ejs");
+
+// tell EJS which layout to use
+app.set('layout', 'layouts/base')
+
+// enable form submission via browser
+app.use(express.urlencoded({
+    extended: true
+}));
 
 // create a connection pool
 const connection = createPool(
@@ -25,43 +35,116 @@ const connection = createPool(
     }
 )
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
+
 
     const todayDate = new Date().toLocaleDateString("en-GB");
-    // the first arg to res.render is the name 
+    // the first arg to res.render is the name
     // of the ejs file to send back to the user
     // and it will be assumed to be in the views folder
-    res.render("landing", {
+    res.render("home", {
         "todayDate": todayDate
     });
 
 });
 
-app.get('/customers', async function(req,res){
+
+app.get('/customers', async function (req, res) {
+
+
     const sql = `
         SELECT * FROM Customers
             JOIN Companies ON
                 Customers.company_id = Companies.company_id
+        ORDER BY Customers.first_name, Customers.last_name
         `
     // connection.query takes in the SQL statement as parameter
     // and returns an array of two elements
     // index 0 is the results
     // index 1 is some metadata
-    const responses = await connection.query({
+    const [customers] = await connection.execute({
         "sql": sql,
         "nestTables": true
     });
-    // res.send(responses[0]);
-    console.log(responses[0])
+
     res.render('customers/index', {
-        customers: responses[0]
+        customers: customers
     })
 })
 
-app.get('/customers/create', async function(req,res){
-    res.render('customers/create')
+
+app.get('/customers/create', async function (req, res) {
+    const [companies] = await connection.execute("SELECT * FROM Companies");
+    const [employees] = await connection.execute("SELECT * FROM Employees");
+
+    // get all the products to display in the form
+    const [products] = await connection.execute("SELECT * FROM Products");
+
+    res.render('customers/create', {
+        companies, employees, products
+    })
 })
 
-app.listen(3000, function(){
+
+// one route to process the form
+app.post('/customers/create', async function (req, res) {
+    // whenever the user has submitted via the form
+    // is in req.body
+    console.log(req.body);
+
+    // a transaction allows us to perform two or more SQL updates or inserts or deletes
+    // and count them as the same atomic operation
+
+    // 1. get a connection instance from the connection pool
+    const conn = await connection.getConnection();
+
+    try {
+        await conn.beginTransaction();
+
+        // do all your SQL mutations (update, insert, delete) here
+        const sql = `
+        INSERT INTO Customers (first_name, last_name, email, company_id, employee_id)
+            VALUES (?, ?, ?, ?, ?);
+    `
+        const [results] = await connection.execute(sql, [
+            req.body.first_name,
+            req.body.last_name,
+            req.body.email,
+            req.body.company_id,
+            req.body.employee_id
+        ]);
+
+        // get the id of the newly created customers
+        const newCustomerId = results.insertId;
+
+        // products will be an array of product ids
+        // p will be the id of the product that the user wants to add
+        if (Array.isArray(req.body.products)) {
+            for (let p of req.body.products) {
+                const sql = `INSERT INTO CustomerProduct (customer_id, product_id) VALUES (?, ?)`;
+                await connection.execute(sql, [newCustomerId, p]);
+            }
+        }
+        // confirm all mutations
+        await conn.commit();
+
+    } catch (e) {
+        await conn.rollback();
+    } finally {
+        await conn.release();
+    }
+
+    res.redirect('/customers')
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
     console.log("Server started");
 })
